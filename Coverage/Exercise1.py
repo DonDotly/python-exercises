d156de35da987ec6c0ea6fae5af17af316993da3
Don Dotly <fonchodotly@gmail.com>
2023-05-22 15:20:27 +0100

    Added python-exercises

---
+++ Coverage/Exercise1.py
@@ -0,0 +1,7 @@
+s = 11
+if s %2 == 0 :
+   print ("The number is divisible by 2" )
+
+else :
+   print (" The Number is not divisible by 2")
+ 
---
+++ Coverage/Exercise2.py
@@ -0,0 +1,5 @@
+name = input( " What is your name") 
+if name == "Acha" :
+    print(" Welcome on Board Acha")
+else:
+    print("Welcome Engineer", name)
